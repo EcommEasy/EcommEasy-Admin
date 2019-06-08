68ac29b94e0e8f89b0dda9b19c7a0df2e18238f4
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2019-06-08 14:23:56 +0530

    Configs Changed

--- config/admin.js
+++ config/admin.js
@@ -2,8 +2,8 @@
 module.exports = {
 	// dashboard UI language
 	language: process.env.LANGUAGE || 'en',
-	apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/api/v1',
-	assetsBaseURL: process.env.ASSETS_BASE_URL || 'http://localhost:3001',
-	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || 'ws://localhost:3001',
+	apiBaseUrl: process.env.API_BASE_URL || 'http://165.22.79.116:3001/api/v1',
+	assetsBaseURL: process.env.ASSETS_BASE_URL || 'http://165.22.79.116:3001',
+	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || 'ws://165.22.79.116:3001',
 	developerMode: process.env.DEVELOPER_MODE || true
 };
